1357da81d3615f9aca5b410c1f4e5ea413f3a5af
Jerome-afk <otienojerome7@gmail.com>
2026-01-22 07:55:18 +0300

    Update: Added main html and feature to submit

---
+++ static/script.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('studentForm');
+const tableBody = document.getElementById('studentTableBody');
+
+// Get student entries from file
+let students = JSON.parse(localStorage.getItem('students')) || [];
+
+function renderTable() {
+    tableBody.innerHTML = '';
+
+    students.forEach(student => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${student.name}</td>
+            <td>${student.age}</td>
+            <td>${student.reg}</td>
+            <td>${student.phone}</td>
+            <td>${student.school}</td>
+            `;
+        tableBody.appendChild(row);
+    });
+}
+
+renderTable(); // Dispalay on page load
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const student = {
+        name: document.getElementById('name').value.trim(),
+        age: document.getElementById('age').value,
+        reg: document.getElementById('reg').value.trim(),
+        phone: document.getElementById('phone').value.trim(),
+        school: document.getElementById('school').value.trim()
+    };
+
+    students.push(student);
+    localStorage.setItem('students', JSON.stringify(students));
+
+    renderTable();
+
+    form.reset();
+});
